2c32b9362729071db68c6f232ed8b2f4fc145bb2
DevelopmentCapitalBank <epsnak@gmail.com>
2023-02-21 11:55:11 +0300

    added two repository

---
+++ RMS.DATA/Repositories/MaskRepository.cs
@@ -0,0 +1,53 @@
+﻿using System.Data;
+using Dapper;
+using RMS.DATA.BaseRepositories;
+using RMS.DATA.Entities;
+
+namespace RMS.DATA.Repositories
+{
+    internal class MaskRepository : IRepositoryStandart<Mask>
+    {
+        #region SQL
+        private static readonly string Update = "UPDATE [Mask] SET MaskTypeId=@MaskTypeId, Content=@Content WHERE MaskId=@MaskId;";
+        private static readonly string Insert = "INSERT INTO [Mask] (MaskTypeId, Content) VALUES (@MaskTypeId, @Content);";
+        private static readonly string Delete = "DELETE FROM [Mask] WHERE MaskId=@MaskId;";
+        private static readonly string Select = "SELECT MaskId, MaskTypeId, Content FROM [Mask];";
+        private static readonly string SqlIdentity = "SELECT last_insert_rowid()";
+        #endregion
+
+        public async Task<Mask> CreateAsync(Mask entity, IDbConnection connection)
+        {
+            connection.Open();
+            var parameters = new DynamicParameters();
+            parameters.Add("MaskTypeId", entity.MaskTypeId);
+            parameters.Add("Content", entity.Content);
+            await connection.ExecuteAsync(Insert, parameters).ConfigureAwait(false);
+
+            int? MaskId = await connection.QueryFirstOrDefaultAsync<int>(SqlIdentity).ConfigureAwait(false);
+            entity.MaskId = MaskId.Value;
+            connection.Close();
+            return entity;
+        }
+
+        public async Task DeleteAsync(Mask entity, IDbConnection connection)
+        {
+            var parameters = new DynamicParameters();
+            parameters.Add("MaskId", entity.MaskId);
+            await connection.ExecuteAsync(Delete, parameters).ConfigureAwait(false);
+        }
+
+        public async Task<IEnumerable<Mask>> ReadAllAsync(IDbConnection connection)
+        {
+            return await connection.QueryAsync<Mask>(Select).ConfigureAwait(false);
+        }
+
+        public async Task UpdateAsync(Mask entity, IDbConnection connection)
+        {
+            var parameters = new DynamicParameters();
+            parameters.Add("MaskTypeId", entity.MaskTypeId);
+            parameters.Add("Content", entity.Content);
+            parameters.Add("MaskId", entity.MaskId);
+            await connection.ExecuteAsync(Update, parameters).ConfigureAwait(false);
+        }
+    }
+}
---
+++ RMS.DATA/Repositories/MaskTypeRepository.cs
@@ -0,0 +1,51 @@
+﻿using System.Data;
+using Dapper;
+using RMS.DATA.BaseRepositories;
+using RMS.DATA.Entities;
+
+namespace RMS.DATA.Repositories
+{
+    internal class MaskTypeRepository : IRepositoryStandart<MaskType>
+    {
+        #region SQL
+        private static readonly string Update = "UPDATE [MaskType] SET Name=@Name WHERE MaskTypeId=@MaskTypeId;";
+        private static readonly string Insert = "INSERT INTO [MaskType] (Name) VALUES (@Name);";
+        private static readonly string Delete = "DELETE FROM [MaskType] WHERE MaskTypeId=@MaskTypeId;";
+        private static readonly string Select = "SELECT MaskTypeId, Name FROM [MaskType];";
+        private static readonly string SqlIdentity = "SELECT last_insert_rowid()";
+        #endregion
+
+        public async Task<MaskType> CreateAsync(MaskType entity, IDbConnection connection)
+        {
+            connection.Open();
+            var parameters = new DynamicParameters();
+            parameters.Add("Name", entity.Name);
+            await connection.ExecuteAsync(Insert, parameters).ConfigureAwait(false);
+
+            int? MaskTypeId = await connection.QueryFirstOrDefaultAsync<int>(SqlIdentity).ConfigureAwait(false);
+            entity.MaskTypeId = MaskTypeId.Value;
+            connection.Close();
+            return entity;
+        }
+
+        public async Task DeleteAsync(MaskType entity, IDbConnection connection)
+        {
+            var parameters = new DynamicParameters();
+            parameters.Add("MaskTypeId", entity.MaskTypeId);
+            await connection.ExecuteAsync(Delete, parameters).ConfigureAwait(false);
+        }
+
+        public async Task<IEnumerable<MaskType>> ReadAllAsync(IDbConnection connection)
+        {
+            return await connection.QueryAsync<MaskType>(Select).ConfigureAwait(false);
+        }
+
+        public async Task UpdateAsync(MaskType entity, IDbConnection connection)
+        {
+            var parameters = new DynamicParameters();
+            parameters.Add("Name", entity.Name);
+            parameters.Add("MaskTypeId", entity.MaskTypeId);
+            await connection.ExecuteAsync(Update, parameters).ConfigureAwait(false);
+        }
+    }
+}
